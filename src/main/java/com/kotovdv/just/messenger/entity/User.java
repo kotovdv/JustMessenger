a09e7e64a3bd6b939e40ed73ebde428b22285398
kotovdv <xeno93@gmail.com>
2017-03-13 22:28:11 +0300

    Fixed wrong equals comparison being used for user entity

--- src/main/java/com/kotovdv/just/messenger/entity/User.java
+++ src/main/java/com/kotovdv/just/messenger/entity/User.java
@@ -46,7 +46,7 @@ public class User {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof User)) return false;
 
         User user = (User) o;
 
